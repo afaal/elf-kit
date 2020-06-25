d6357b9bb7a7e08ffa1ab7125d371a6fe6ab118d
Andreas larsen <afal@pop-os.localdomain>
2020-06-25 09:42:59 +0200

    Remove useless comment

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use std::result::*;
 fn main() {
 
     let bin = elf::read_file("test/testBin").expect("Failed with"); 
-
+    
+    
     bin.write_file("test/testBin.test");
-    // bin.write_file("./test/testBin.test"); 
 }
