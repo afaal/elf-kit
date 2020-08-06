82f6d2869e6883bcfb9987fb8f18eddcf4b72233
Andreas larsen <afal@pop-os.localdomain>
2020-08-06 14:21:34 +0200

    Add elf_editor crate for manipulating files

---
+++ elf_editor/src/lib.rs
@@ -0,0 +1,6 @@
+use elf::Elf; 
+
+// remove all section headers from an elf object
+pub fn remove_shdrs(elf: &mut Elf) {}
+
+pub fn remove_ne_phdrs(elf: &mut Elf) {}
--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,11 @@ use std::result::*;
 
 fn main() {
 
-    let bin = elf::read_file("test/testBin").expect("Failed with"); 
+    let mut bin = elf::read_file("test/testBin").expect("Failed to open file"); 
     
+    elf_editor::remove_shdrs(&mut bin); 
+    elf_editor::remove_ne_phdrs(&mut bin); 
+
     
-    bin.write_file("test/testBin.test");
+    bin.write_file("test/testBin.new"); 
 }
